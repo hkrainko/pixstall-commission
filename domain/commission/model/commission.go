34abea1c30b8689a81d668f16980cd529fab3ab2
gary_chan <gary_chan@chiefgroup.com.hk>
2020-12-21 16:59:17 +0800

    no message

--- domain/commission/model/commission.go
+++ domain/commission/model/commission.go
@@ -1,5 +1,30 @@
 package model
 
+import (
+	"time"
+)
+
 type Commission struct {
-	ID string
+	ID               string
+	OpenCommissionID string
+	ArtistID         string
+	ClientID         string
+	BidPrice         float64
+	Size             Size
+	RequestDate      time.Time
+	CompleteTime     time.Time
+	State            CommissionState
+}
+
+type CommissionState string
+
+const (
+	CommissionStatePending        CommissionState = "P"
+	CommissionStateRejectByArtist CommissionState = "RJ_BY_ARTIST"
+	CommissionStateRejectByClient CommissionState = "RJ_BY_CLIENT"
+)
+
+type Size struct {
+	Width  float64
+	Height float64
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module pixstall-commission
 
 go 1.15
+
+require (
+	github.com/aws/aws-sdk-go v1.36.12 // indirect
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/google/wire v0.4.0 // indirect
+	github.com/streadway/amqp v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.4.4 // indirect
+)
